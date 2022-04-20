4739cfbaf585fe0dba6ebb2d5089b3b05009daaa
ANIL CAN ÖZDEMİR <anilcanozdemir1020@gmail.com>
2022-04-20 15:34:51 +0300

    init

---
+++ src/ManavKasaProgrami.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * @author ANIL CAN ÖZDEMİR
+ *
+ */
+public class ManavKasaProgrami {
+    public static void main(String[] args) {
+        System.out.println();
+        Scanner input =new Scanner(System.in);
+
+    }
+}
